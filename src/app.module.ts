ffdafdd5b0bb1e18323e4aeedb2d86ce9d687496
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-06 22:09:32 +0100

    feat: create UserEntity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from './prisma/prisma.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { User } from "@prisma/client";
+
+export class UserEntity implements User {
+    id: number;
+    email: string;
+    name: string;
+    admin: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}

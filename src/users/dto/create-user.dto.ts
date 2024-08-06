ce86bd20f71f92c1c31fb00d5c185bcae63b8b13
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-06 22:22:04 +0100

    feat: creating validation data in dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+/* eslint-disable prettier/prettier */
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from "class-validator";
+/* eslint-disable prettier/prettier */
+export class CreateUserDto {
+    @IsString({ message: 'Name must be a string' })
+    @IsNotEmpty({ message: 'Name is required' })
+    name: string;
+
+    @IsString({ message: 'Email must be a string' })
+    @IsEmail()
+    @IsNotEmpty({ message: 'Email is required' })
+    email: string;
+
+    @IsBoolean(
+
+    )
+    admin: boolean;
+}

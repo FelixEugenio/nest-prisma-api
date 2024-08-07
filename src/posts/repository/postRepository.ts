38b4e2c4305e544cf13481d6a87d1f1fdfac3e57
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-07 21:12:36 +0100

    feat: create postRepository feature

---
+++ src/posts/repository/postRepository.ts
@@ -0,0 +1,49 @@
+/* eslint-disable prettier/prettier */
+import { PrismaService } from "src/prisma/prisma.service";
+import { PostEntity } from "../entities/post.entity";
+import { CreatePostDto } from "../dto/create-post.dto";
+import { UpdatePostDto } from "../dto/update-post.dto";
+import { Post } from '@prisma/client';
+
+/* eslint-disable prettier/prettier */
+export class PostRepository {
+    constructor(
+        private readonly prismaService: PrismaService
+    ) {}
+
+   async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+        return this.prismaService.post.create({
+          data: createPostDto
+        })
+      }
+
+     async  findAll(): Promise<PostEntity[]> {
+        return this.prismaService.post.findMany();
+      }
+
+    async  findOne(id: number): Promise<PostEntity> {
+        return this.prismaService.post.findUnique({
+          where: {
+            id,
+          }
+        });
+      }
+
+    async  update(id: number, updatePostDto: UpdatePostDto): Promise<PostEntity> {
+        return this.prismaService.post.update({
+          where: {
+            id,
+          },
+          data: updatePostDto
+        });
+      }
+
+    async  remove(id: number): Promise<PostEntity> {
+        return this.prismaService.post.delete({
+          where: {
+            id,
+          }
+        });
+
+      }
+}

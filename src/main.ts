9194bfcb17d0f6bc4abe5e806b1b179f8cd2e479
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-07 20:07:23 +0100

    feat: create UniqueConstraintError

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {  ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { UnauthorizedInterceptor } from './common/errors/interceptors/Unautorized.intercepetors.error';
 import { NotFoundInterceptor } from './common/errors/interceptors/NotfoundInterceptor';
+import { ConflictInterceptor } from './common/errors/interceptors/ConflictInterceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/DatabaseInterceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,8 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
   //app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor())
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotFoundInterceptor())
   await app.listen(3001);

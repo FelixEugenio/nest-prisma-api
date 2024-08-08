8fc725a54d30f81acc74d878b01d650e3d3de328
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-08 21:19:16 +0100

    refactor: imports

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,15 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreatePostDto {
+    @ApiProperty({ description: ' title of post' })
     @IsString()
     @IsNotEmpty()
     title: string;
 
+    @ApiProperty({ description: ' content of post' })
     @IsString()
     @IsOptional()
     content: string;
 
+    @ApiProperty({ description: 'email of author' })
     @IsEmail()
     authorEmail: string;
 }

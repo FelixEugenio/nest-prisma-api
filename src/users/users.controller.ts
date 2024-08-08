46e27c0525e5120c4a3adf3c988ebc22e6f41a20
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-08 21:25:25 +0100

    refactor: imports

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,11 +3,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiResponse({ status: 201, description: 'The record has been successfully created.' })
+  @ApiForbiddenResponse({ status: 403, description: 'Forbidden.' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
